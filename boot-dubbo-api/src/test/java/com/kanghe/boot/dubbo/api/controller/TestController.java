9ec37b69cd5dbe017bb3eac3d7a0be4dba1f4558
wujun <18813918091@163.com>
2019-06-01 12:07:02 +0800

    dubbo+zookeeper+springboot整合

---
+++ boot-dubbo-api/src/test/java/com/kanghe/boot/dubbo/api/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.kanghe.boot.dubbo.api.controller;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/5/28 10:18
+ * @Description:
+ */
+public class TestController {
+}
---
+++ boot-dubbo-provider/src/test/java/com/kanghe/boot/dubbo/provider/service/TestService.java
@@ -0,0 +1,9 @@
+package com.kanghe.boot.dubbo.provider.service;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/5/28 10:18
+ * @Description:
+ */
+public class TestService {
+}
